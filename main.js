02f35522ae6ca41b15ba12025ac3b02500ce622f
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2025-03-23 10:03:24 +0300

    init commit

---
+++ main.js
@@ -0,0 +1,31 @@
+export function knightTravails(start, end) {
+  const directions = [
+    [-2, -1],
+    [-2, 1],
+    [-1, -2],
+    [-1, 2],
+    [1, -2],
+    [1, 2],
+    [2, -1],
+    [2, 1],
+  ];
+
+  const queue = [[start, [[0, 0]]]];
+  const visited = new Set();
+  visited.add(`${start[0]},${start[1]}`);
+  while (queue.length > 0) {
+    const [position, path] = queue.shift();
+    for (const direction of directions) {
+      const newX = position[0] + direction[0];
+      const newY = position[1] + direction[1];
+      if (newX === end[0] && newY === end[1]) {
+        return path.concat([[newX, newY]]);
+      }
+      if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8 && !visited.has(`${newX},${newY}`)) {
+        queue.push([[newX, newY], path.concat([[newX, newY]])]);
+        visited.add(`${newX},${newY}`);
+      }
+    }
+  }
+  return null;
+}
---
+++ test.js
@@ -0,0 +1,3 @@
+import { knightTravails } from "./main.js";
+let path = knightTravails([0, 0], [7, 7]);
+console.log(path);
